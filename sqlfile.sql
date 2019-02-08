c7f62f24536d838f7017c671d4f458157c4955a3
tatafella <47183467+tatafella@users.noreply.github.com>
2019-02-08 16:13:23 +0100

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -11,3 +11,22 @@ CREATE DATABASE "databaseTPDocker"
 COMMENT ON DATABASE "databaseTPDocker"
   IS 'Création d''une base de données pour le test du tp docker postgres';
 
+CREATE TABLE Client ( 
+  id       INTEGER NOT NULL PRIMARY KEY, 
+  nom  VARCHAR(256), 
+  prenom  VARCHAR(256), 
+  age     INTEGER
+  );
+
+CREATE TABLE Commande ( 
+  id       INTEGER NOT NULL PRIMARY KEY, 
+  montant  INTEGER
+
+);
+
+INSERT INTO Client (id,nom, prenom, age)
+    VALUES (1,'TATA', 'Fella', 24), (2,'Ghada', 'CHTIOUI', 23),(3,'Rostom', 'VEGAPUNK',24),(4,'Akram', 'METALAOUI', 24);
+
+
+INSERT INTO Commande (id, montant)
+    VALUES (1,235), (2,2100)
