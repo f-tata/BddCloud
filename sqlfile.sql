3905a16caf558afbf4d356d3a1d87d9b12f010fb
tatafella <47183467+tatafella@users.noreply.github.com>
2019-02-08 18:35:08 +0100

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -17,15 +17,6 @@ ALTER DATABASE "databaseTPDocker" OWNER TO postgres;
 
 \connect "databaseTPDocker"
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- TOC entry 2861 (class 0 OID 0)
