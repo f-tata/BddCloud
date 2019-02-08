30a3e7146e7bd253c1d11b05a908a2194bd4980c
tatafella <47183467+tatafella@users.noreply.github.com>
2019-02-08 18:32:58 +0100

    Update sqlfile.sql

--- sqlfile.sql
+++ sqlfile.sql
@@ -5,18 +5,6 @@
 -- Dumped from database version 10.6 (Debian 10.6-1.pgdg90+1)
 -- Dumped by pg_dump version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
 
--- Started on 2019-02-08 18:28:01 CET
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- TOC entry 2860 (class 1262 OID 20601)
 -- Name: databaseTPDocker; Type: DATABASE; Schema: -; Owner: postgres
